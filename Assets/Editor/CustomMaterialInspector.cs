98618c7520216b87290d381944eb6ce470c86d49
nakamura001 <polluxtk@gmail.com>
2013-03-16 01:57:11 +0900

    add comment.

--- Assets/Editor/CustomMaterialInspector.cs
+++ Assets/Editor/CustomMaterialInspector.cs
@@ -21,9 +21,11 @@ public class CustomMaterialInspector : MaterialEditor
 		// see if redify is set, then show a checkbox
 		bool redify = keyWords.Contains ("REDIFY_ON");
 		EditorGUI.BeginChangeCheck ();
+		//  ここの記述が Inspector に反映される。今回は「Redify material」という名前のトグル(bool)のデータ
 		redify = EditorGUILayout.Toggle ("Redify material", redify);
 		if (EditorGUI.EndChangeCheck ()) {
 			// if the checkbox is changed, reset the shader keywords
+			//  チェックボックスの状態でShader内のキーワード (定数)を REDIFY_ON にするか REDIFY_OFF にするかを切り替える
 			var keywords = new List<string> { redify ? "REDIFY_ON" : "REDIFY_OFF"};
 			targetMat.shaderKeywords = keywords.ToArray ();
 			EditorUtility.SetDirty (targetMat);
